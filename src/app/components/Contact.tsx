884ff86281c60bcd2f61d93a7e7d3c728ed96649
jvfontouraj <jvfontouraj@gmail.com>
2024-06-15 10:21:06 -0300

    fix contact colors

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -17,28 +17,29 @@ export function Contact({ lang }: { lang: string }) {
       <h2 className="text-center text-3xl 2xl:text-4xl">{contact.title}</h2>
       <div className="flex w-full flex-col items-center justify-around gap-7 md:flex-row md:gap-0">
         <Link
-          href={'https://github.com/jvfontouraj'}
-          className="flex w-fit items-center gap-3 rounded-[10px] border border-white bg-neutral-800 px-6 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.5)] transition-shadow duration-300 hover:shadow-white"
+          href={'https://www.linkedin.com/in/joaovfjardim/'}
+          className="flex w-fit items-center gap-3 rounded-[10px] border border-neutral-500 px-6 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.5)] transition-shadow duration-300 hover:shadow-white dark:border-white dark:bg-neutral-800"
         >
-          <GitHubLogoIcon className="h-5 w-5 md:h-9 md:w-9" />
-          <div className="text-lg md:text-xl">/jvfontouraj</div>
+          <LinkedInLogoIcon className="h-5 w-5 md:h-9 md:w-9" />
+          <div className="text-lg md:text-xl">/joaovfjardim</div>
         </Link>
         <Link
           target="_blank"
           href={
             'https://mail.google.com/mail/?view=cm&fs=1&to=jvfjardim@gmail.com&su=Contato%20atrav%C3%A9s%20do%20site%20&body=Ol%C3%A1%2C'
           }
-          className="flex w-fit items-center gap-3 rounded-[10px] border border-white bg-neutral-800 px-6 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.5)] transition-shadow duration-300 hover:shadow-white"
+          className="flex w-fit items-center gap-3 rounded-[10px] border border-neutral-500 px-6 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.5)] transition-shadow duration-300 hover:shadow-white dark:border-white dark:bg-neutral-800"
         >
           <EnvelopeClosedIcon className="h-5 w-5 md:h-9 md:w-9" />
           <div className="text-lg md:text-xl">jvfjardim@gmail.com</div>
         </Link>
+
         <Link
-          href={'https://www.linkedin.com/in/joaovfjardim/'}
-          className="flex w-fit items-center gap-3 rounded-[10px] border border-white bg-neutral-800 px-6 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.5)] transition-shadow duration-300 hover:shadow-white"
+          href={'https://github.com/jvfontouraj'}
+          className="flex w-fit items-center gap-3 rounded-[10px] border border-neutral-500 px-6 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.5)] transition-shadow duration-300 hover:shadow-white dark:border-white dark:bg-neutral-800"
         >
-          <LinkedInLogoIcon className="h-5 w-5 md:h-9 md:w-9" />
-          <div className="text-lg md:text-xl">/joaovfjardim</div>
+          <GitHubLogoIcon className="h-5 w-5 md:h-9 md:w-9" />
+          <div className="text-lg md:text-xl">/jvfontouraj</div>
         </Link>
       </div>
     </section>
